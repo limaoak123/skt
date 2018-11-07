bc8e076eaa2344ecbd8f98dea9ca6ad23c0b256a
Limao <381206193@qq.com>
2018-11-07 10:18:00 +0800

    新品完成，商品列表基本完成，还需路由传参

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
-    
+         <router-view></router-view> 
   </div>
 </template>
 
 
 <style scoped>
-  @import '../static/reset.css'
+  @import '../static/reset.css';
 </style>
 
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+
 }
 </script>
 
--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import NewsGood from '../views/NewsGood.vue';
+import PhoneList from '../views/PhoneList.vue';
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      
+      path: '/newsgood',
+      component: NewsGood
+    },
+    {
+      path: '/phonelist',
+      component: PhoneList
     },
   ]
 })
